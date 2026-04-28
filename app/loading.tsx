a07751d6704bd87c11637ff2894d3bd390638c07
cyberentityz <cyberentityz@gmail.com>
2026-04-28 22:26:48 +0530

    fixed merge conflict

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 
 const Loading = () => {
@@ -9,18 +8,4 @@ const Loading = () => {
     </div>
   )
 }
-
-=======
-import React from 'react'
-
-const Loading = () => {
-  return (
-     <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-linear-to-br from-purple-600 via-blue-600 to-indigo-600">
-      <div className="w-16 h-16 border-4 border-white/30 border-t-white rounded-full animate-spin"></div>
-      <p className="mt-4 text-white font-bold text-lg">Loading...</p>
-    </div>
-  )
-}
-
->>>>>>> ebb6d838d7f0b54540424e8cb2a814b3de4e67c5
 export default Loading
